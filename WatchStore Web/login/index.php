f2dacb941551d5b8f616c333ca936923d1a53d69
rafiramzi <79634000+rafiramzi@users.noreply.github.com>
2025-06-18 22:02:32 +0700

    Folder Stucture

--- WatchStore Web/login/index.php
+++ WatchStore Web/login/index.php
@@ -1,5 +1,5 @@
 <?php
-include("db.php");
+include("../db.php");
 
 ob_start();
 session_start();
--- WatchStore Web/product/product.php
+++ WatchStore Web/product/product.php
@@ -3,7 +3,7 @@ include("../db.php");
 
 session_start();
 if (!isset($_SESSION['username'])) {
-    header('Location: /login.php');
+    header('Location: /login');
     exit;
 }
 
